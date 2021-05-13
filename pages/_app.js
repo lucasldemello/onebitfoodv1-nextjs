2de809292f5bb55c251b18e10b43c3040770cb07
Lucas de Melo <lucas.melo@mpgo.mp.br>
2021-05-12 22:44:53 -0300

    Commit com os primieros passos das aulas.

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,23 @@
-import '../styles/globals.css'
+import Head from 'next/head';
+import '../styles/globals.scss';
+import Header from '../components/Header';
+import Container from 'react-bootstrap/Container';
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Head>
+        <title>OneBitFood V2</title>
+        <link ref="icon" href="/favicon.icon"></link>
+      </Head>
+      <main>
+        <Header/>
+        <Container className='mt-5'>
+          <Component {...pageProps} />
+        </Container>
+      </main>
+    </>
+  )
 }
 
 export default MyApp
